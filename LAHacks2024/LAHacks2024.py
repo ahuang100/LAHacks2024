0051b40109724254b65fffbace9431b66ca45b3e
Johnny Wu <jwu9636@gmail.com>
2024-04-21 00:30:08 -0700

    transition page

--- LAHacks2024/LAHacks2024.py
+++ LAHacks2024/LAHacks2024.py
@@ -9,6 +9,7 @@ from LAHacks2024.pages import abilities
 from LAHacks2024.pages import room
 from LAHacks2024.pages import convinceme
 from LAHacks2024.pages import yousaid
+from LAHacks2024.pages import transition
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction/"
 filename = f"{config.app_name}/{config.app_name}.py"
@@ -30,6 +31,6 @@ app.add_page(naming.naming_page, route="/naming")
 app.add_page(abilities.index, route="/abilities")
 app.add_page(convinceme.convincemepage, route="/convinceme")
 app.add_page(yousaid.yousaid_page, route="/yousaid")
-
+app.add_page(transition.index, route="/transition")
 app.add_page(naming.naming_page, route="/naming")
 app.add_page(room.room_page)
